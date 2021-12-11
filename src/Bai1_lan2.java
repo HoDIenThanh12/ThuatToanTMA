f7faad57eb5e16505babfeb3954662f01b58d51f
thanh <hodienthanh123@gmail.com>
2021-12-12 00:29:15 +0700

    update Ex.one

--- src/Bai1_lan2.java
+++ src/Bai1_lan2.java
@@ -3,39 +3,47 @@ import java.util.Scanner;
 public class Bai1_lan2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n=10, k=8;
-//        System.out.print("Nhập n: ");
-//        n = sc.nextInt();
+        int n, k;
+        System.out.print("Nhập n: ");
+        n = sc.nextInt();
 
-//        System.out.print("Nhập k: ");
+        System.out.print("Nhập k: ");
         sc = new Scanner(System.in);
-//        k = sc.nextInt();
+        k = sc.nextInt();
         System.out.print("Nhập mảng: \n");
-        int arr[] = {1,5,7,8,1,2,3,6,9,7};
-//        for (int i = 0; i < n; i++) {
-//            System.out.print("a[" + i + "]: ");
-//            arr[i] = sc.nextInt();
-//        }
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("a[" + i + "]: ");
+            arr[i] = sc.nextInt();
+        }
         Bai1_lan2.quickSort(arr, 0, n - 1);
         for (int i = 0; i < n; ++i)
             System.out.print(arr[i] + " ");
         int mid=n/2;
-        int position = n/2;
         System.out.print("\n");
         for(int i = 0 ; i< mid ; i++){
-            if(arr[i] + arr[mid] < 8){
-                position=mid ++;
-                i--;
-                position++;
-            }else {
-                if(arr[i] + arr[mid] > 8){
-                    mid--;
-                    i--;
+            if(mid < n && mid >-1){
+                if(arr[i] + arr[mid] < 8){
+                    if((arr[i]+arr[mid+1])  <=k ){
+                        mid ++;
+                        i--;
+                    }
+
                 }else {
-                    System.out.print("("+arr[i] + "," +arr[mid]+")");
-                    mid--;
+                    if(arr[i] + arr[mid] > 8){
+                        if((arr[i]+arr[mid-1]) >=k ){
+                            mid--;
+                            i--;
+                        }
+
+                    }else {
+                        System.out.print("("+arr[i] + "," +arr[mid]+")");
+                        mid--;
+                    }
                 }
             }
+            else
+                break;
         }
     }
     public static int  partition(int arr[], int low, int high) {
