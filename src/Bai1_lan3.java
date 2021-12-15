90035bc32287c0e0a3af5c1b881339428fc000e1
thanh <hodienthanh123@gmail.com>
2021-12-15 23:31:46 +0700

    TheEnd

--- src/Bai1_lan3.java
+++ src/Bai1_lan3.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Bai1_lan3 {
     public static void main(String[] args) {
@@ -9,28 +6,25 @@ public class Bai1_lan3 {
         int n,k;
         System.out.print("Input totals numbers n: ");
         n = sc.nextInt();
-
         System.out.print("Sum k: ");
         sc = new Scanner(System.in);
         k = sc.nextInt();
         System.out.print("Input Arrays : \n");
         int arr[] = new int[n];
-        Map<Integer, Integer> arrCheck = new HashMap<>();
-        int [] l = new int[1];
         for (int i = 0; i < n; i++) {
             System.out.print("a[" + i + "]: ");
             arr[i] = sc.nextInt();
-            if(arrCheck.containsKey(arr[i])){
-                arrCheck.put(arr[i], arrCheck.get(arr[i]) + 1);
-            }else{
-                arrCheck.put(arr[i], 1);
-            }
         }
-        for(int i = 0 ; i < n ; i++){
-            if( arrCheck.get(k - arr[i]) != null && arrCheck.get(k - arr[i])>=1 && arrCheck.get( arr[i])>=1  ){
-                System.out.println("("+arr[i] + " , " + (k - arr[i])+")");
-                arrCheck.put(k - arr[i], arrCheck.get(k - arr[i]) - 1);
-                arrCheck.put(arr[i], arrCheck.get(arr[i]) - 1);
+        Arrays.sort(arr);
+        int l=0, r=arr.length-1;
+        while (l <r){
+            int sum =arr[l]+arr[r];
+            if(sum ==k){
+                System.out.print("("+arr[l]+","+arr[r]+")");
+                l++;r--;
+            }else {
+                if(sum >k) r--;
+                else  l++;
             }
         }
     }
